29b212b67cad7360ca5aaa72821c84a957f09bcc
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-01 13:40:54 +0700

    refactoring code for form view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,10 +1,11 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def index
         @articles = Article.all
     end
 
     def show
-        @article = Article.find(params[:id])
     end
 
     def new
@@ -12,7 +13,7 @@ class ArticlesController < ApplicationController
     end
 
     def create
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             # redirect_to article_path(@article) # commonly use
             flash[:notice] = "Article was created succesffuly."
@@ -25,12 +26,10 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = "Article was updated successfully."
             redirect_to @article
         else
@@ -39,7 +38,6 @@ class ArticlesController < ApplicationController
     end
 
     def destroy
-        @article = Article.find(params[:id])
         @article.destroy
         redirect_to articles_path
     end
@@ -47,7 +45,11 @@ class ArticlesController < ApplicationController
 
     private
 
-    # def article_params
-    #     params.require(:article).permit(:title, :description)
-    # end
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
 end
