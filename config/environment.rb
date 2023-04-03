375fae99610c9ff107d05697832bf9a8a07a3953
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-03 21:44:11 +0700

    create partial view for errors, add styling to errors message with button close, disable rails error field with error using environment config

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+# to disable adding html div field error iin rails html
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+  html_tag.html_safe
+end
