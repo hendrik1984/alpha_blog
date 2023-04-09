ab690b1bb242cbe44f971ae3c5d3e3b22d60696d
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-09 15:12:12 +0700

    move helper to application controller and update article current user, update root home page to articles listing page when user already logged in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::Base
+  
+  helper_method :current_user
+  def current_user
+    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  # explanation, at first current_user set to application_helper.rb
+  # so that only available for views only, not at contoller.
+  # with current_user set to application_controller.rb it will available
+  # to controller, but now not available at views
+  # to solve this solution we can add helper_method :current_user
+  # now it will avaiable both views and controller
+
+  helper_method :logged_in?
+  def logged_in?
+    !!current_user
+  end
 end
