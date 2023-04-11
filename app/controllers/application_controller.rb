488936afe3d1b7f7ceae8af9195156a0891dad0a
hendrik-ubuntu <li.zhiqiang2011@gmail.com>
2023-04-11 14:52:53 +0700

    add user delete navigation, add user dependent articles destroy and add action destroy in controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,7 +10,15 @@ class ApplicationController < ActionController::Base
   # with current_user set to application_controller.rb it will available
   # to controller, but now not available at views
   # to solve this solution we can add helper_method :current_user
-  # now it will avaiable both views and controller
+  # so it make the function logged_in? available to views
+  # application_controller.rb => function will show only to xxx_controller.rb
+  # application_helper.rb => function will show only to xxx.html.erb only
+  # to make available on views add helper_method in application_controller.rb
+  # example:
+  # helper_method :logged_in? => this code will make views can use logged_in?
+  # def logged_in? => this function only available on controller only
+  #   !!current_user
+  # end 
 
   helper_method :logged_in?
   def logged_in?
